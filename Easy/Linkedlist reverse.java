1a4baace94d0899fb2891d0be61a6760c9f98c2f
bassam207 <70178718+bassam207@users.noreply.github.com>
2025-09-02 19:35:36 +0300

    Update Linkedlist reverse.java

--- Easy/Linkedlist reverse.java
+++ Easy/Linkedlist reverse.java
@@ -61,3 +61,22 @@ class Solution {
        return prev;
     }
 }
+
+
+
+// with recursion 
+class Solution {
+    public ListNode reverseList(ListNode head) {
+
+       if (head == null || head.next == null)
+       {
+        return head;
+       }
+
+       ListNode newhead = reverseList(head.next);
+
+       head.next.next = head;
+       head.next = null;
+       return newhead;
+    }
+}
