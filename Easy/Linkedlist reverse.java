e8293474822941763e4c9681395515ce083762f5
bassam207 <70178718+bassam207@users.noreply.github.com>
2025-09-02 19:05:28 +0300

    Create Linkedlist reverse.java

---
+++ Easy/Linkedlist reverse.java
@@ -0,0 +1,63 @@
+/**
+
+prev(null)    curr
+    ↓         ↓
+   null     [1] → [2] → [3] → [4] → null
+
+prev
+ ↓
+[1] → null      curr
+                ↓
+              [2] → [3] → [4] → null
+
+prev
+ ↓
+[2] → [1] → null     curr
+                     ↓
+                   [3] → [4] → null
+
+prev
+ ↓
+[3] → [2] → [1] → null     curr
+                            ↓
+                          [4] → null
+
+prev
+ ↓
+[4] → [3] → [2] → [1] → null
+curr = null
+
+*/
+
+
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+
+       ListNode prev = null;
+       ListNode curr = head;
+       ListNode nextn ;
+
+
+       while( curr != null)
+       {
+        
+            nextn = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nextn;
+       }
+
+       return prev;
+    }
+}
